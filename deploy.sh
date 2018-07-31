ba7cd48a633e00d0ad797bd1a11718155022ad48
sangmin <sang93423@gmail.com>
2018-07-31 21:14:42 +0900

    .secrets status확인 추가;

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+pipenv lock --requirements > requirements.txt
 git add -A
 echo "전체파일 staged 영역에 추가"
 git add -f .secrets/
-echo "git add 막아논 파일 강제로 추가"
+git status
+echo "git add 안되는 파일 강제로 추가"
 eb deploy --profile jsm-eb-user --staged
 echo "staged 영역에서 배포시작"
 git reset HEAD .secrets/ requirements.txt
